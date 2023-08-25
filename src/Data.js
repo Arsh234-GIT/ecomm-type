f54947682af3a3e7ea838f7abc4c728c6b4f96ec
Arsh234-GIT <ahamadarsh92@gmail.com>
2023-08-25 23:34:33 +0530

    sdf

--- src/Data.js
+++ src/Data.js
@@ -5,8 +5,8 @@ const Data = [
         title: "XOX70 EXTRAS",
         description: "The XOX70 Extras offer a range of enhancements for keyboard enthusiasts. Choose between Solder PCB or Hot-Swap PCB options, each providing distinct benefits for assembly. Additionally, you have the choice of Aluminum Full Plate, Aluminum Half Plate, FR4 Full Plate, FR4 Half Plate, POM Full Plate, and PC Full Plate, each designed to elevate your typing experience by providing stability and versatility. These components are meticulously crafted to cater to various preferences, ensuring that your keyboard-building journey is both enjoyable and rewarding. Whether you're seeking a solder or hot-swap PCB, or considering the advantages of different plate materials like aluminum, FR4, or POM, these options empower you to craft a keyboard that aligns perfectly with your preferences.",
         price: 3400,
-        coverImg: "/keyboards/keyboard1.webp",
-        hoverImg: "/keycaps/key1.webp",
+        coverImg: "/TypingTrove/keyboards/keyboard1.webp",
+        hoverImg: "/TypingTrove/keycaps/key1.webp",
 
 
     },
@@ -16,8 +16,8 @@ const Data = [
         title: "PBTFANS™ RESONANCE",
         description: "Resonance draws its inspiration from the iconic Braun calculator ET series, infusing the timeless design with modern durability through KBDfans' innovative Doubleshot PBT/metal process. This melding of classic aesthetics and long-lasting quality results in a keycap set that exudes both style and resilience.Constructed from premium PBT material, these keycaps ensure a satisfying tactile experience while retaining their pristine appearance over time. The Cherry profile further enhances typing comfort and familiarity. Utilizing the Doubleshot production method, each keycap boasts dual-layered legends that are resistant to fading, ensuring that your keyboard's appearance remains as sharp as its performance.",
         price: 2000,
-        coverImg: "/keyboards/keyboard2.webp",
-        hoverImg: "/keycaps/key2.webp",
+        coverImg: "/TypingTrove/keyboards/keyboard2.webp",
+        hoverImg: "/TypingTrove/keycaps/key2.webp",
 
     },
     {
@@ -26,8 +26,8 @@ const Data = [
         title: " PBTFANS™ SPARK R2 ",
         description: "Experience gaming at its best with the PBTFANS™ SPARK R2, now available for pre-order. Engineered to perfection, the SPARK R2 is your gateway to uninterrupted gaming sessions, thanks to its advanced cooling system that keeps your hardware running optimally even during intense gameplay. Say goodbye to overheating concerns and hello to a sleek and durable design that seamlessly integrates with your setup, enhanced by customizable RGB lighting. With simple installation and silent performance, the SPARK R2 is compatible with various PC cases and hardware configurations, making it a must-have addition for both seasoned builders and newcomers",
         price: 8500,
-        coverImg: "/keyboards/keyboard3.webp",
-        hoverImg: "/keycaps/key3.webp",
+        coverImg: "/TypingTrove/keyboards/keyboard3.webp",
+        hoverImg: "/TypingTrove/keycaps/key3.webp",
 
     }
     ,
@@ -36,8 +36,8 @@ const Data = [
         title: " PRE-ORDER ROSEWATER KEYCAPS",
         description: " Indulge in the beauty of nature with our exquisite Rosewater Keycaps, available now for pre-order. Take a stroll through a tranquil garden as soft pink tones transport you to the idyllic embrace of a warm spring day. Crafted with meticulous attention to detail, these keycaps seamlessly blend elegance and functionality, promising a delightful typing experience that's both visually captivating and comfortable.Elevate your keyboard aesthetics with the gentle allure of Rosewater. Compatible with a wide range of mechanical keyboards, the Rosewater Keycaps are designed for effortless installation, making it easy for you to personalize your workspace. Whether you're a creative enthusiast, a dedicated writer, or a tech-savvy individual, these keycaps offer a unique opportunity to infuse your keyboard with the enchanting essence of blooming gardens. ",
         price: 8500,
-        coverImg: "/keyboards/keyboard4.webp",
-        hoverImg: "/keycaps/key4.webp",
+        coverImg: "/TypingTrove/TypingTrove/keyboards/keyboard4.webp",
+        hoverImg: "/TypingTrove/keycaps/key4.webp",
 
     },
     {
@@ -46,8 +46,8 @@ const Data = [
         title: "PBTFANS™ DOUBLESHOT BOW",
         description: "Introducing the PBTfans™ Doubleshot BOW keycap set, now available in B-Stock: Customer Return. Crafted from high-quality PBT material and featuring a Cherry profile, these keycaps offer a comfortable and familiar typing experience. The doubleshot production method ensures long-lasting legends that won't fade over time. Manufactured by PBTfans™, this keycap set is compatible with Cherry MX switches and MX-style clones, providing versatile options for keyboard enthusiasts. Elevate your keyboard aesthetics and functionality with the PBTfans™ Doubleshot BOW keycap set – a perfect choice for those seeking quality and style. Please note that this package includes only the keycaps, and not the keyboard itself.  Cherry MX switches and MX-style clones  give the discription of the above product in one paragraph not subpoints not bulletpoints based on above data  ",
         price: 2300,
-        coverImg: "/keyboards/keyboard5.webp",
-        hoverImg: "/keycaps/key5.webp",
+        coverImg: "/TypingTrove/keyboards/keyboard5.webp",
+        hoverImg: "/TypingTrove/keycaps/key5.webp",
 
     },
     {
@@ -56,8 +56,8 @@ const Data = [
         title: "PBTFANS™ DOUBLESHOT/TRIPLESHOT NEON",
         description: "PBTfans™ ABS keycap set, crafted with quality and design in mind. With a Cherry profile and featuring a doubleshot production method, these keycaps ensure durable legends that won't fade over time. The inclusion of tripleshot for JP Sub-Legends alpha kit further enhances the visual appeal. Manufactured by PBTfans™, this keycap set offers compatibility with Cherry MX switches and MX-style clones, catering to a wide range of mechanical keyboards. It's essential to note that this package includes only the keycaps, and not the keyboard itself. Elevate your typing experience and keyboard aesthetics with the PBTfans™ ABS keycap set – a perfect fusion of style and functionality.We got Feedback from customers that some keys which use 2U Stabilizers might have issues with Costar Stabilizers due to the form of the keycaps",
         price: 3000,
-        coverImg: "/keyboards/keyboard6.webp",
-        hoverImg: "/keycaps/key6.webp",
+        coverImg: "/TypingTrove/keyboards/keyboard6.webp",
+        hoverImg: "/TypingTrove/keycaps/key6.webp",
 
     },
     {
@@ -66,8 +66,8 @@ const Data = [
         title: " KEYCHRON PBT KEYCAP SETS - ISO",
         description: "Crafted from highly durable PBT plastic and adorned with dye-sublimated legends, these keycaps are built to withstand the demands of even the most intense users. With a robust thickness of 1.4mm, these keycaps offer a perfect balance between resilience and a satisfying tactile feel, ensuring a comfortable typing experience that lasts. The dye-sublimated legends are meticulously designed to preserve the clarity of backlighting, infusing your keyboard with a vibrant glow that's both functional and aesthetically pleasing.Designed in the popular OEM profile, these keycaps not only deliver exceptional comfort during extended typing sessions but also contribute to the visual allure of your keyboard setup. Each keycap is carefully crafted to fit snugly, resulting in a seamless and cohesive appearance across your entire keyboard.",
         price: 4000,
-        coverImg: "/keyboards/keyboard7.webp",
-        hoverImg: "/keycaps/key7.webp",
+        coverImg: "/TypingTrove/keyboards/keyboard7.webp",
+        hoverImg: "/TypingTrove/keycaps/key7.webp",
 
     },
     {
@@ -76,8 +76,8 @@ const Data = [
         title: "PBTFANS™ DOUBLESHOT SPARK",
         description: "Introducing the PBTfans™ Doubleshot Spark keycap set, a testament to quality and design. Crafted with durable PBT material and utilizing the popular Cherry profile, these keycaps are designed to endure extended use while maintaining a comfortable typing experience. The doubleshot production method ensures legends won't fade over time, retaining their clarity and legibility. Designed by BIIP, this set offers compatibility with Cherry MX switches and MX-style clones, allowing you to enhance your keyboard with a touch of personal style and functionality. Please note that this package includes only the keycaps, and not the keyboard itself. Elevate your typing experience with the PBTfans™ Doubleshot Spark keycap set, a fusion of form and function.",
         price: 1800,
-        coverImg: "/keyboards/keyboard8.webp",
-        hoverImg: "/keycaps/key8.webp",
+        coverImg: "/TypingTrove/keyboards/keyboard8.webp",
+        hoverImg: "/TypingTrove/keycaps/key8.webp",
 
     }
     ,
@@ -88,8 +88,8 @@ const Data = [
         title: "KIT ADAM ",
         description: "Introducing ADAM: The inaugural masterpiece from KBDcraft, a custom 60% mechanical keyboard that artfully blends the Minecraft retro aesthetic with modern innovation. ADAM features a brick-built case adhering to LEGO norms, embodying durability and visual charm. Its revolutionary gasket mount ensures a tactile and satisfying typing experience. Crafted for upgradability, ADAM's Core design and QMK firmware empower users to customize keys and functions. With meticulous attention to detail, ADAM presents a solid foundation for future customization. Embrace the keyboard revolution and infuse your workspace with the nostalgic allure of ADAM's design.",
         price: 7000,
-        coverImg: "/keyboards/keyboard9.webp",
-        hoverImg: "/keycaps/key9.webp",
+        coverImg: "/TypingTrove/keyboards/keyboard9.webp",
+        hoverImg: "/TypingTrove/keycaps/key9.webp",
 
     }
     ,
@@ -99,8 +99,8 @@ const Data = [
         title: "STAMP PBT KEYCAPS",
         description: "While sorting through old things, I turn to a letter from an old friend many years ago and remember the age of sending messages by letter.At that time, the letters always carry the sincerity and desire, because of the slow transmission of letters, let us know how to cherish time, in each letter to express our deep care and love.But now with advanced technology and rapid information dissemination, that feeling no longer exists. Hence the idea of creating stamps.",
         price: 2500,
-        coverImg: "/keyboards/keyboard10.webp",
-        hoverImg: "/keycaps/key10.webp",
+        coverImg: "/TypingTrove/keyboards/keyboard10.webp",
+        hoverImg: "/TypingTrove/keycaps/key10.webp",
 
     }
     ,
@@ -110,8 +110,8 @@ const Data = [
         title: "SAKUYA KEYCAPS R2  ",
         description: "Introducing Sakuya Keycaps R2 – a perfect blend of form and function. Crafted with precision, these keycaps are made from PBT material using the Dye Sub process, ensuring long-lasting legends that won't fade. Designed in the popular Cherry Profile and boasting a thickness of 1.7mm, they provide both comfort and durability. With 157 keys, the set is meticulously crafted to be compatible with a wide array of layouts, catering to your unique preferences.Anticipate an elevated typing experience as these keycaps are set to ship in late Q3 2023. Don't miss out on upgrading your keyboard with the exquisite Sakuya Keycaps R2.",
         price: 9000,
-        coverImg: "/keyboards/keyboard11.webp",
-        hoverImg: "/keycaps/key11.webp",
+        coverImg: "/TypingTrove/keyboards/keyboard11.webp",
+        hoverImg: "/TypingTrove/keycaps/key11.webp",
 
     }
     ,
@@ -121,8 +121,8 @@ const Data = [
         title: " DCS ALCHEMY KEYCAPS",
         description: "DCS Alchemy revives a timeless colorway in a classic profile. This compact base kit provides support for various layouts including 60%, 65%, 75%, and TKL, making it versatile for different typing preferences. Additionally, it offers support for split backspace, basic ISO, and a 7u bottom row, ensuring compatibility with a wide range of keyboard configurations. Embrace nostalgia and modern functionality in one with DCS Alchemy – a keyboard set that brings a touch of the past to the convenience of the present.",
         price: 10999,
-        coverImg: "/keyboards/keyboard12.webp",
-        hoverImg: "/keycaps/key12.webp",
+        coverImg: "/TypingTrove/keyboards/keyboard12.webp",
+        hoverImg: "/TypingTrove/keycaps/key12.webp",
 
     }
 
