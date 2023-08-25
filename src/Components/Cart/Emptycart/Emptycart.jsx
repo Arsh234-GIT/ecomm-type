4afb95ec32b5f611c8da5729c26ce28d9468a504
Arsh234-GIT <ahamadarsh92@gmail.com>
2023-08-26 00:19:55 +0530

    l

--- src/Components/Cart/Emptycart/Emptycart.jsx
+++ src/Components/Cart/Emptycart/Emptycart.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./EmptyCart.css"; // Update the file name here
+import "./Emptycart.css"
 import { useNavigate } from "react-router-dom";
 
 const EmptyCart = () => {
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faTwitter, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
-import "./Footer.css";
+import "./Footer.css"
 
 function Footer() {
   return (
