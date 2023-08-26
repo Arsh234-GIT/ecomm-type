167d4de0ae61009fc9f228ad9d9931aadac6bd9b
Arsh234-GIT <ahamadarsh92@gmail.com>
2023-08-26 13:09:24 +0530

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
+ 
   plugins: [react()],
+  base:/ecommerce-key/
 
-  base: "typing-ecommerce/"
+
 
 })
